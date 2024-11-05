d0d7276a5efd96d3dff3d49060d71e76ea415556
sil <silamlak@gmail.com>
2024-11-05 15:12:19 +0300

    Enable CORS for webhook requests

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,13 @@
 """
 Registers the webhook blueprint to handle incoming webhook requests.
 """
+from os import getenv
 from flask import Flask
+from flask_cors import CORS
 from api.webhook import webhook_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(webhook_blueprint)
+
+allowed_origin = getenv("CORS_ALLOWED_ORIGIN", "*")
+CORS(app, origins=[allowed_origin], methods=["POST"])
